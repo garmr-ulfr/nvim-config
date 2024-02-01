ece0371ca21e85476ff44bd2463a2f9c0d4bc982
garmr <pdixon117@gmail.com>
2024-02-01 10:58:13 -0800

    remap goplay, custom settings for md, txt files

--- lua/garmr/configs/goplay.lua
+++ lua/garmr/configs/goplay.lua
@@ -1,7 +1,7 @@
 local gp = require('goplay')
 gp.setup({output_mode = 'raw'})
 
-vim.keymap.set('n', '<leader>gpt', function ()
+vim.keymap.set('n', '<leader>gop', function ()
 	local bufExist = pcall(vim.api.nvim_buf_get_name, gp._activeBuf)
 	if gp._activeBuf and bufExist then
 		vim.cmd.bdelete{gp._activeBuf, bang = true}
@@ -10,5 +10,5 @@ vim.keymap.set('n', '<leader>gpt', function ()
 		gp.goPlaygroundOpen()
 	end
 end)
-vim.keymap.set('n', '<leader>gpe', ':GPExec<CR>')
-vim.keymap.set('n', '<leader>gpf', ':GPExecFile<CR>')
+vim.keymap.set('n', '<leader>goe', ':GPExec<CR>')
+vim.keymap.set('n', '<leader>gof', ':GPExecFile<CR>')
